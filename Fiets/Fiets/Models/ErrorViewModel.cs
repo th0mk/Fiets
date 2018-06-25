a21da1806431ca841f45b0681b487315657a8fb5
Thom <thomkaptein@gmail.com>
2018-06-25 22:03:39 +0200

    initial commit

---
+++ Fiets/Fiets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fiets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Fiets/Fiets/gulpfile.js
@@ -0,0 +1,57 @@
+﻿/// <binding Clean='clean' ProjectOpened='default' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rimraf = require("rimraf");
+var merge = require('merge-stream');
+
+gulp.task("minify", function () {
+
+    var streams = [
+        gulp.src(["wwwroot/js/*.js"])
+            .pipe(uglify())
+            .pipe(concat("app.min.js"))
+            .pipe(gulp.dest("wwwroot/lib/site"))
+    ];
+
+    return merge(streams);
+});
+
+// Dependency Dirs
+var deps = {
+    "jquery": {
+        "dist/*": ""
+    },
+    "bootstrap": {
+        "dist/**/*": ""
+    },
+    "popper.js": {
+        "dist/**/*": ""
+    }
+};
+
+gulp.task("clean", function (cb) {
+    return rimraf("wwwroot/lib/", cb);
+});
+
+gulp.task("scripts", function () {
+
+    var streams = [];
+
+    for (var prop in deps) {
+        console.log("Prepping Scripts for: " + prop);
+        for (var itemProp in deps[prop]) {
+            streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+                .pipe(gulp.dest("wwwroot/lib/" + prop + "/" + deps[prop][itemProp])));
+        }
+    }
+
+    return merge(streams);
+});
+
+gulp.task("default", gulp.series('clean', 'scripts', 'minify'));
