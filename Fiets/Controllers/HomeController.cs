57f313f3e9bcbe1214476302d22f65c2f9415150
Thom <thomkaptein@gmail.com>
2018-06-26 18:52:02 +0200

    fix

--- Fiets/Controllers/HomeController.cs
+++ Fiets/Controllers/HomeController.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Fiets.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Fiets.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
             return View();
