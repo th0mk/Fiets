2d4bc5fed718f08372387ee5e9bcfcf1c415848e
Thom Kaptein <thomkaptein@gmail.com>
2018-07-01 20:13:56 +0200

    ride overview

--- Fiets/Controllers/HomeController.cs
+++ Fiets/Controllers/HomeController.cs
@@ -8,14 +8,54 @@ using Fiets.Models;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using Fiets.Data;
+using Fiets.ViewModels;
 
 namespace Fiets.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var currentRides = db.Rides
+                .Where(w => !w.RideFinished)
+                .Select(s => new RideListItem
+                {
+                    RideID = s.RideID,
+                    RideStartKm = s.RideStartKm,
+                    RideFinished = s.RideFinished,
+                    User = db.Users.FirstOrDefault(f => f.Id == s.UserID).UserName
+                }).ToList();
+
+            var lastRides = db.Rides
+                .Where(w => w.RideFinished)
+                .OrderBy(o => o.RideEndTimeUtc)
+                .Take(5)
+                .Select(s => new RideListItem
+                {
+                    RideID = s.RideID,
+                    RideEndKm = s.RideEndKm,
+                    RideEndTimeUtc = s.RideEndTimeUtc,
+                    RideStartKm = s.RideStartKm,
+                    RideStartTimeUtc = s.RideStartTimeUtc,
+                    RideFinished = s.RideFinished,
+                    User = db.Users.FirstOrDefault(f => f.Id == s.UserID).UserName
+                }).ToList();
+
+
+            var viewmodel = new IndexViewModel
+            {
+                CurrentRides = currentRides,
+                LastRides = lastRides
+            };
+
+            return View(viewmodel);
         }
 
         public IActionResult About()
---
+++ Fiets/Models/HomeViewModels.cs
@@ -0,0 +1,37 @@
+﻿using Fiets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiets.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<RideListItem> CurrentRides { get; set; }
+
+        public List<RideListItem> TopRides { get; set; }
+
+        public List<RideListItem> LastRides { get; set; }
+    }
+
+    public class RideListItem
+    {
+        [Key]
+        public int RideID { get; set; }
+
+        public string User { get; set; }
+
+        public bool RideFinished { get; set; }
+
+        public DateTime RideStartTimeUtc { get; set; }
+
+        public int RideStartKm { get; set; }
+
+        public int? RideEndKm { get; set; }
+
+        public DateTime? RideEndTimeUtc { get; set; }
+    }
+}
